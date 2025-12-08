2e53958432e29211f1170c0ae4e5ee95600eb14f
Aybüke Gökçe Yavaş <gokcemmy16@gmail.com>
2025-12-08 17:12:52 +0300

    Add project files.

---
+++ SalonYöneticisi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalonYöneticisi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
