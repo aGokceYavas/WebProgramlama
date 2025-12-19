635ea9bd13067566a343a392e6acc39aab816516
Aybüke Gökçe Yavaş <gokcemmy16@gmail.com>
2025-12-19 17:13:49 +0300

    API ile admin paneline raporlar sayfası eklendi

---
+++ SalonYöneticisi/Controllers/RaporlarController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace GymManagementApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RaporlarController : Controller
+    {
+        // GET: Raporlar/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
